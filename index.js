926297521dc24c094d2c235d12ca90e89fa5575d
Nawan Ernandes <nawanernandess@gmail.com>
2024-08-04 15:25:00 -0300

    Melhorias finalizadas.
    -- Atualiza layout;
    -- Atualiza estilo;
    -- Adiciona responsividade.

--- index.js
+++ index.js
@@ -1,39 +1,49 @@
-function contar() {
-  var ini = window.document.getElementById("txti");
-  var fim = window.document.getElementById("txtf");
-  var passo = window.document.getElementById("txtp");
-  let res = window.document.getElementById("result");
+let result = document.getElementById("result");
 
-  if (
-    ini.value.length == 0 ||
-    fim.value.length == 0 ||
-    passo.value.length == 0
-  ) {
-    res.innerHTML = "[ERRO] Impossivel contar!";
-    //window.alert('[ERRO] Faltam dados!')
+function startCounter() {
+  var start = document.getElementById("start");
+  var end = document.getElementById("end");
+  var step = document.getElementById("step");
+
+  if (step.value <= 0 && start.value.length > 0 && end.value.length > 0) {
+    step.value = 1;
+  }
+
+  detectValueError(start);
+  detectValueError(end);
+
+  if (start.value.length == 0 || end.value.length == 0) {
+    return (result.innerHTML = "Preencha os campos para contar!");
+  }
+
+  result.innerHTML = "Contando: ";
+
+  count(start.value, end.value, step.value);
+}
+
+function count(start, end, step) {
+  var startValue = Number(start);
+  var endValue = Number(end);
+  var stepValue = Number(step);
+  var count;
+
+  if (startValue < endValue) {
+    for (count = startValue; count <= endValue; count += stepValue) {
+      result.innerHTML += ` ${count}, `;
+    }
   } else {
-    res.innerHTML = "Contando: <br>";
-    var i = Number(ini.value);
-    var f = Number(fim.value);
-    var p = Number(passo.value);
-
-    if (p <= 0) {
-      window.alert("Passo invalido! Considerando PASSO 1");
-      p = 1;
+    for (count = startValue; count >= endValue; count -= stepValue) {
+      result.innerHTML += `${count}, `;
     }
+  }
 
-    if (i < f) {
-      //contagem crescente
-      for (c = i; c <= f; c += p) {
-        res.innerHTML += ` ${c} \u{1f449}`;
-      }
-      res.innerHTML += `\u{1f3c1}`;
-    } else {
-      // Contagem decrescente
-      for (c = i; c >= f; c -= p) {
-        res.innerHTML += `${c} \u{1f449}`;
-      }
-      res.innerHTML += `\u{1f3c1}`;
-    }
+  result.innerHTML = result.innerHTML.slice(0, -2) + ".";
+}
+
+function detectValueError(elem) {
+  if (elem.value.length == 0) {
+    elem.classList.add("error");
+  } else {
+    elem.classList.remove("error");
   }
 }
